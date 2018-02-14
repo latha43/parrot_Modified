b3ada7a4cc9bd38cd732e9c36e300758c0a0a6b0
Gopakumar Gopinathan <gopakumar.gopinathan@ust-global.com>
2018-02-14 14:33:54 +0530

    test case with pytest

---
+++ tests/test_bitbucket_api.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+import pytest
+
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here + '/../ansible/bb_roles')
+
+from module_utils.bitbucket_core import BitBucket
+
+@pytest.fixture
+def bb_interface():
+    return BitBucket
+
+
+def test_projects(bb_interface):
+    assert bb_interface.is_project_existing('test')
+
+
